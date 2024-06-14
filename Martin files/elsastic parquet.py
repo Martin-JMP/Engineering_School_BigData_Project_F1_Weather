fff1ddb4eba62fd1386e8930e8bd9a225254a484
Martin-JMP <115220121+Martin-JMP@users.noreply.github.com>
2024-06-14 15:20:45 +0200

    Add files via upload

--- Martin files/elsastic parquet.py
+++ Martin files/elsastic parquet.py
@@ -7,11 +7,26 @@ import numpy as np
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Lire le fichier Parquet
-file_path = r'D:\combined_data.parquet'
+file_path = r'D:\part-00000-afd7655e-9993-4900-999d-3ff934ddb4f0-c000.snappy.parquet'
 df = pd.read_parquet(file_path)
+# Configurer Pandas pour afficher toutes les lignes et toutes les colonnes
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+# Afficher le DataFrame complet
+print(df)
 
 # Supprimer les colonnes contenant des valeurs NaN
-df_clean = df.dropna(axis=1)
+df_clean = df.fillna(0)
+print(df_clean)
+
+# Supprimer la colonne 'best_lap_time'
+df_clean = df_clean.drop(columns=['best_lap_time'])
+print("DataFrame après suppression de la colonne 'best_lap_time':")
+print(df_clean)
+
 
 # Initialiser le client Elasticsearch avec l'authentification de base
 client = Elasticsearch(
@@ -33,9 +48,10 @@ def pandas_df_to_elasticsearch(df, index_name):
     helpers.bulk(client, actions)
 
 # Nom de l'index Elasticsearch
-index_name = "test_f1_V6"
+index_name = "test_f1_V9"
 
 print("début de l'indéxation")
+
 
 # Indexer les données
 try:
@@ -43,3 +59,11 @@ try:
     print("Indexation réussie")
 except helpers.BulkIndexError as e:
     print(f"Erreur lors de l'indexation : {e.errors}")
+
+# Vérifier si l'index a été créé avec succès et afficher le nombre de documents indexés
+if client.indices.exists(index=index_name.lower()):
+    print(f"L'index {index_name.lower()} existe.")
+    count = client.count(index=index_name.lower())['count']
+    print(f"Nombre de documents dans l'index {index_name.lower()}: {count}")
+else:
+    print(f"L'index {index_name.lower()} n'existe pas.")
